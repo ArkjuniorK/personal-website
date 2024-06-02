02132f11b5dd759d800743a8dd02b42ab7a37868
ArkjuniorK <created.arkjuniork@gmail.com>
2024-06-02 14:33:21 +0800

    feat: add description

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -13,10 +13,15 @@ const about: ICommand = {
     "     |.o '.|      \x1b[1mborn\x1b[0m: December, 17th 1999",
     "     |'._.'|      \x1b[1mmbti\x1b[0m: INFP (quiet, open minded and imaginative)",
     "     |     |      \x1b[1mfields\x1b[0m: Fullstack & Backend Engineer",
-    "   ,'|  |  |`.    \x1b[1meducation\x1b[0m: Bachelors of Informatics",
+    "   ,'|  |  |`.    \x1b[1mtitles\x1b[0m: Bachelor of Informatics",
     "  /  |  |  |  \\   \x1b[1mtech_stack\x1b[0m: Go, Node, GNU/Linux, Docker, etc.",
     "  |,-'--|--'-.|               (use `tech` command for more info)",
     "",
+    "┌───",
+    "│ An open-minded and adaptable INFP with proven experience in developing web",
+    "  applications.\x1b[0m \x1b[1mCreative, loyal, and a team player\x1b[0m, I maintain high-quality",
+    "  work through best practices.                                                │",
+    "                                                                           ───┘",
   ],
   func: (term: Terminal) => {
     const c = about.contents.join("\r\n");
