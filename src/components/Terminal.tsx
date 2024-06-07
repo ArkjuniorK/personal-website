0687b7ef73b115d037b7cc28db9867c6bfebc44b
ArkjuniorK <created.arkjuniork@gmail.com>
2024-06-07 11:53:17 +0800

    feat: use ResizeObserver to observe the change of terminal container's sizes

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -1,5 +1,5 @@
 import { promptTerminal, runTerminalCommand } from "../util/terminal";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ITheme, Terminal } from "@xterm/xterm";
 import { FitAddon } from "@xterm/addon-fit";
 import commands from "../commands";
@@ -15,15 +15,30 @@ const theme: ITheme = {
   background: "rgb(0, 0, 0, 0)",
 };
 
-function TerminalWindow() {
+// These variables would be initialize when component
+// is mount and in order to make consistent we need
+// to place it outside of the component.
+// Otherwise everytime component
+// re-render the value would
+// be undefined
+let command: string = "";
+let terminal: Terminal;
+let fitAddon: FitAddon;
+
+// Whenever terminal container's size is changes, reload
+// the fitAddon to match the current size of container.
+// This way we doesn't need to use React useEffect
+const resizeObs = new ResizeObserver(function () {
+  fitAddon && fitAddon.fit();
+});
+
+function Term(props: any) {
   const termRef = useRef(null);
+  const [maximize, setMaximize] = useState(false);
 
-  let command = "";
-  let terminal: Terminal;
-  let fitAddon: FitAddon;
-
+  // Init the terminal, addon, and their
+  // event when component is mount.
   useEffect(() => {
-    fitAddon = new FitAddon();
     terminal = new Terminal({
       theme: theme,
       fontSize: 13,
@@ -34,9 +49,11 @@ function TerminalWindow() {
       scrollOnUserInput: true,
     });
 
+    fitAddon = new FitAddon();
+
     terminal.loadAddon(fitAddon);
     terminal.open(termRef.current as any);
-    fitAddon.fit();
+    resizeObs.observe(termRef.current as any);
 
     commands.greet.func(terminal);
 
@@ -81,17 +98,40 @@ function TerminalWindow() {
     });
 
     return () => {
-      fitAddon.dispose();
       terminal.dispose();
+      fitAddon.dispose();
     };
   }, []);
 
-  return (
-    <section className="relative w-full lg:w-2/3 xl:w-2/3 h-full lg:h-3/6 xl:h-2/3 p-2 z-20 lg:mx-auto lg:my-auto transition-all ease-in-out duration-500">
-      <div className="bg-dark relative h-full w-full rounded-2xl  ring-2 ring-white/60 text-black dark:text-white p-3 animate__animated animate__fadeInDown animate__delay-1s">
-        <div ref={termRef} className="w-full h-full text-black"></div>
+  return props.isOpen ? (
+    <section
+      className={`relative w-full h-full ${maximize ? "" : "lg:w-2/3 xl:w-2/3 lg:h-3/6 xl:h-2/3"} p-2 z-20 lg:mx-auto lg:my-auto transition-all ease-in-out duration-500`}
+    >
+      <div className="bg-dark relative h-full w-full rounded-2xl ring-4 ring-white/80 text-black dark:text-white animate__animated animate__zoomIn animate__delay-1s">
+        <div className="bg-dark absolute z-10 w-full rounded-t-xl lg:flex px-3 py-2 space-x-2 justify-start sm:hidden">
+          <button className="bg-red-400 p-2 rounded-full" disabled></button>
+          <button
+            className="bg-yellow-400 p-2 rounded-full"
+            disabled
+          ></button>
+          <button
+            onClick={() => setMaximize((v) => !v)}
+            className="bg-green-400 p-2 rounded-full"
+          ></button>
+        </div>
+        <div className="px-3 pb-3 py-7 w-full h-full relative">
+          <div id="terminal" ref={termRef} className="relative w-full h-full"></div>
+        </div>
       </div>
     </section>
+  ) : null;
+}
+
+function TerminalWindow() {
+  return (
+    <div className="h-full w-full relative flex">
+      <Term isOpen={true} />
+    </div>
   );
 }
 
